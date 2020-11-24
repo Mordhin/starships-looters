3d0084a62c82d5b36af235fa5735900d5ef666f3
Julien Corion <astram59@gmail.com>
2020-11-24 14:45:06 +0100

    end_after_start variables fixed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,8 +10,8 @@ class Booking < ApplicationRecord
   private
 
   def end_after_start
-    if end_date < start_date
-      errors.add(:end_date, "must be after the start date")
+    if date_end < date_start
+      errors.add(:date_end, "must be after the start date")
     end
   end
 end
