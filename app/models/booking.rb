3cbb1c849c82ff28ef7fbc042a95ee7d4b627c8e
Julien Corion <astram59@gmail.com>
2020-11-24 14:24:26 +0100

    add validates end_after_start

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,17 @@ class Booking < ApplicationRecord
   belongs_to :ship
 
   validates :user, :ship, presence: true
-  validates :date_start, :date_end, presence: true, if: -> {:date_start < :date_end} 
+  validates :date_start, :date_end, presence: true
+  validates :end_after_start
   validates :crew_size, :date_start, :date_end, numericality: { only_integer: true }
+
+  private
+
+  def end_after_start
+    return if end_date.blank? || date_start.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
