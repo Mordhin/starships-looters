c3537da9862f81ae6daba134893ddd1940003633
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-11-24 17:50:24 +0100

    Routes ships and booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,27 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @ship = Ship.find(params[:ship_id])
+  end
+
+  def create
+    @booking = Booking.new(set_bookings)
+    @ship = Ship.find(params[:ship_id])
+
+    @owner = @ship.user
+    @booking.ship = @ship
+    @booking.user = @owner
+
+    if @booking.save
+      redirect_to bookings_path, notice: 'Your request has been created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_bookings
+    params.require(:bookings).permit(:crew_size, :date_start, :date_end)
+  end
 end
