a854380f70bef6e66a851e8b30366ff20cca75a3
adrien cousin <adrien.cousin@lilo.org>
2020-11-24 17:55:41 +0100

    Created booking routes (with nest in ship routes)

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :ships
+
+  resources :bookings, only: %i[index edit update destroy]
+  resources :ships do
+    resources :bookings, only: %i[new create]
+  end
 end
