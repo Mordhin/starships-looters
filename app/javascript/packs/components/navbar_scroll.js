be128f5d849912556ece2cc3e79c338b129fe7c3
Stephane Briclot <stephane.briclot@gmail.com>
2020-11-24 16:14:42 +0100

    Basic home page with a cleaned Ships selection list

--- app/javascript/packs/components/navbar_scroll.js
+++ app/javascript/packs/components/navbar_scroll.js
@@ -2,7 +2,7 @@ const navbarScroll = () => {
   window.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar-lewagon");
     console.log(navbar);
-    navbar.classList.toggle("scrolled", window.scrollY > 250);
+    navbar.classList.toggle("scrolled", window.scrollY > 100);
   });
 };
 
