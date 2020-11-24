1889da2c4715c643f098786057f3f61cea226fd7
Julien Corion <astram59@gmail.com>
2020-11-24 16:26:49 +0100

    capitalize and better available

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -5,5 +5,18 @@ class ShipsController < ApplicationController
 
   def show
     @ship = Ship.find(params[:id])
+    @available_status = available_status(@ship.available)
+    
   end
+
+  private
+
+  def available_status(available)
+    if available
+      return "Ce vaisseau est disponible"
+    else
+      return "Désolé, ce vaisseau n'est pas disponible"
+    end
+  end
+  
 end
