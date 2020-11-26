359713b238a2949a76b340859dee26cc0853c127
adrien cousin <adrien.cousin@lilo.org>
2020-11-26 14:52:17 +0100

    Added booking amount update based on calendar update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,6 +31,7 @@ import "bootstrap";
 import { navbarScroll } from '../components/navbar_scroll';
 import { displayShipDetails } from '../components/ss_show_details';
 import "../plugins/flatpickr"
+import { updateBookingAmount } from '../components/update_booking_amount.js'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
@@ -43,4 +44,5 @@ document.addEventListener('turbolinks:load', () => {
     conjunction: " and ",
     defaultDate: ["today", new Date().fp_incr(7)]
   });
+  updateBookingAmount();
 });
