d63bedb8af7dad46cfd7c23921ca8d3059b6f483
adrien cousin <adrien.cousin@lilo.org>
2020-11-26 23:25:54 +0100

    Implemented static support troll page and action buttons tweaking after merging with sbriclot branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/support', to: 'pages#support', as: 'support'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bookings, only: %i[index edit update destroy] do
     member do
