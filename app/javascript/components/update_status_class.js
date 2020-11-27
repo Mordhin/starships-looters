13580175e207a97d5bf15480dd67a509e2840474
Stephane Briclot <stephane.briclot@gmail.com>
2020-11-27 15:24:35 +0100

    JS cleaning code

--- app/javascript/components/update_status_class.js
+++ app/javascript/components/update_status_class.js
@@ -1,10 +1,8 @@
 const updateStatusClass = () => {
   const statuses = document.querySelectorAll('.status');
-  console.log(statuses);
   if (statuses) {
     statuses.forEach(status => {
       const statusText = status.innerText;
-      console.log(statusText);
       status.parentElement.classList.add(statusText.toLowerCase());
     });
 
