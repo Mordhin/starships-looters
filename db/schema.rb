5905c395340d4ef28d07fc0f67962bd8f61cdc01
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-11-27 08:20:17 +0100

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_061809) do
+ActiveRecord::Schema.define(version: 2020_11_27_071416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_11_27_061809) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "total_amount"
+    t.integer "price_cents", default: 0, null: false
+    t.string "checkout_session_id"
     t.index ["ship_id"], name: "index_bookings_on_ship_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
