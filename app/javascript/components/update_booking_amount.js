6e9a2c81196dd67a6a5d11492bfd2296ad3aed27
adrien cousin <adrien.cousin@lilo.org>
2020-11-27 01:28:19 +0100

    Added +1 to totalDays

--- app/javascript/components/update_booking_amount.js
+++ app/javascript/components/update_booking_amount.js
@@ -11,7 +11,7 @@ const updateBookingAmount = () => {
     calendar.addEventListener("change", () => {
       startDate = Date.parse(document.querySelector('.datepicker').value.substring(0,10));
       endDate = Date.parse(document.querySelector('.datepicker').value.substring(14));
-      totalDays = (endDate - startDate) / 86400000;
+      totalDays = (endDate - startDate) / 86400000 + 1;
       price = parseInt(pricePerDay,10) * parseInt(totalDays, 10);
       if (document.querySelector('.datepicker').value.length > 10) {
         document.getElementById("booking_submit").value = `Book for ${totalDays} day(s) and ${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} ₹`
