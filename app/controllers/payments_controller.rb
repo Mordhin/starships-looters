a837004badcc1e1c452ece2677768cdf7757d61f
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-11-27 12:44:57 +0100

    Created a payment controller and a new payment view

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,7 @@
+class PaymentsController < ApplicationController
+  class PaymentsController < ApplicationController
+    def new
+      @order = current_user.orders.where(state: 'pending').find(params[:order_id])
+    end
+  end
+end
