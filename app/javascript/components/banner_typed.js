1db9be3034aa27c6983af52f64f7fbb90ed9e21b
Stephane Briclot <stephane.briclot@gmail.com>
2020-11-27 14:03:29 +0100

    Avoid error message due to typedjs script on all pages except home

--- app/javascript/components/banner_typed.js
+++ app/javascript/components/banner_typed.js
@@ -1,14 +1,17 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["The best spaceship rent service of the universe!"],
-    typeSpeed: 50,
-    loop: true,
-    fadeOut: true,
-    fadeOutDelay: 1,
-    showCursor: false
-  });
+  const typedFiled = document.getElementById("banner-typed-text")
+  if (typedFiled) {
+    new Typed('#banner-typed-text', {
+      strings: ["The best spaceship rent service of the universe!"],
+      typeSpeed: 50,
+      loop: true,
+      fadeOut: true,
+      fadeOutDelay: 1,
+      showCursor: false
+    });
+  }
 }
 
 export { loadDynamicBannerText };
